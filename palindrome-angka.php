a9448eddb1c9de96abe3e5dd8cecb25736736e02
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2020-07-30 00:26:19 +0700

    menyelesaikan tugas 1 palindrome angka

---
+++ palindrome-angka.php
@@ -0,0 +1,32 @@
+<?php
+
+function palindrome($string)
+{
+    $hasil = false;
+
+    if (strrev($string) == $string) {
+        $hasil = true;
+    }
+
+    return $hasil;
+}
+
+function palindrome_angka($angka)
+{
+    if ($angka >= 0 && $angka <= 8) {
+        return $angka += 1;
+    }
+
+    while (palindrome($angka) == false) {
+        $angka += 1;
+    }
+
+    return $angka;
+}
+
+// TEST CASES
+echo palindrome_angka(8) . "<br>"; // 9
+echo palindrome_angka(10) . "<br>"; // 11
+echo palindrome_angka(117) . "<br>"; // 121
+echo palindrome_angka(175) . "<br>"; // 181
+echo palindrome_angka(1000); // 1001
