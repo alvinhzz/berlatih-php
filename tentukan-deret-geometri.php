4a3713ee981d923e5b912eddddd7377cc26448fa
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2020-07-30 03:48:56 +0700

    menyelesaikan tugas menentukan deret no.5-atitmatika dan no.6-geometri

---
+++ tentukan-deret-geometri.php
@@ -0,0 +1,19 @@
+<?php
+function tentukan_deret_geometri($arr) {
+// kode di sini
+$suku_arit = $arr[1] / $arr[0];
+
+    for ($i = 1; $i < sizeof($arr); $i++) {
+        $jarak = $arr[$i] / $arr[$i - 1];
+        if ($jarak != $suku_arit) {
+            return "false <br>";
+        }
+    }
+    return "true <br>";
+}
+//TEST CASES
+echo tentukan_deret_geometri([1, 3, 9, 27, 81]); // true
+echo tentukan_deret_geometri([2, 4, 8, 16, 32]); // true
+echo tentukan_deret_geometri([2, 4, 6, 8]); // false
+echo tentukan_deret_geometri([2, 6, 18, 54]); // true
+echo tentukan_deret_geometri([1, 2, 3, 4, 7, 9]); //false
